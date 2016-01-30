be94991b6c3b1645409619d2ff26a87ddb7d9a34
mazodan <danmazo32@gmail.com>
2016-01-30 14:22:39 +0800

    Init commit

---
+++ Assignment/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtNum = New System.Windows.Forms.TextBox
+        Me.btnExecute = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a Number"
+        '
+        'txtNum
+        '
+        Me.txtNum.Location = New System.Drawing.Point(100, 10)
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(42, 20)
+        Me.txtNum.TabIndex = 1
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(148, 8)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(75, 23)
+        Me.btnExecute.TabIndex = 2
+        Me.btnExecute.Text = "Execute"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(245, 523)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.txtNum)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+    Friend WithEvents btnExecute As System.Windows.Forms.Button
+
+End Class
---
+++ Assignment/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub btnExecute_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExecute.Click
+        Dim y As Integer = 40
+        For x As Integer = 1 To Val(txtNum.Text)
+            Dim tb As New TextBox
+            tb.Name = "txtbox" + x.ToString()
+            tb.Location() = New Point(20, y)
+            Me.Controls.Add(tb)
+            y = y + 30
+        Next
+        Dim btn As New Button
+        btn.Name = "btnAdd"
+        btn.Text = "Add"
+        btn.Location() = New Point(140, 40)
+        AddHandler btn.Click, AddressOf AddNumbers
+        Me.Controls.Add(btn)
+    End Sub
+    Private Sub AddNumbers(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim sum As Integer = 0
+        For Each ctrl As Control In Me.Controls
+            If ctrl.GetType Is GetType(TextBox) Then
+                sum = sum + Val(ctrl.Text)
+            End If
+        Next
+        MessageBox.Show(sum - Val(txtNum.Text))
+    End Sub
+
+End Class
